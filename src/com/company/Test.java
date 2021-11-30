4682af263fac06f47f8b46f303006feb326b87f7
huziquan <1510217648@qq.com>
2021-11-30 23:44:20 +0800

    更新了单例模式下的双重检查，但还是有点问题

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Test {
     public static void main(String[] args){
-        System.out.println("这次我在家里面电脑更新11月22日");
+        System.out.println("这次我在家里面电脑更新11月30日");
     }
 }
