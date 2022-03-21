022b97a9e10f09d9e047ccea438f9d55ad0fdd6f
huziquan <1510217648@qq.com>
2022-03-21 09:56:03 +0800

    增加了部分测试题

--- src/LeeCode/Main.java
+++ src/LeeCode/Main.java
@@ -3,9 +3,9 @@ package LeeCode;
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        String str="pwwkew";
-        Solution3 sol=new Solution3();
-        int max=sol.lengthOfLongestSubstring(str);
-        System.out.println("最长的长度："+max);
+
+        Soletion1344 sol=new Soletion1344();
+        double comNum=sol.angleClock(12,50);
+        System.out.println("结果："+comNum);
     }
 }
